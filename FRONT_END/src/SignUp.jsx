284b008c15d08fb93f1a499e2934bcc4844802cf
AhmadMansoor764 <am.web.dev2024@gmail.com>
2026-05-16 15:38:19 +0430

    added opens dashboard when sign up

--- FRONT_END/src/SignUp.jsx
+++ FRONT_END/src/SignUp.jsx
@@ -34,6 +34,7 @@ const SignUp = () => {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify(data),
       });
 
